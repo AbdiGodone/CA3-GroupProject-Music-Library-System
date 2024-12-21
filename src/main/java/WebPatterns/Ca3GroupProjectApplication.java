5fe2c2be12c3b8596d3e1f05ed08af4a73e25087
abdi godone <157476641+AbdiGodone@users.noreply.github.com>
2024-12-21 18:08:30 +0000

    Initial commit

---
+++ src/main/java/WebPatterns/Ca3GroupProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.ca3groupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ca3GroupProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ca3GroupProjectApplication.class, args);
+	}
+
+}
